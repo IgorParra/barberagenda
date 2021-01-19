8b0414df47e26e2c49f6f1349e5b76693ab00a55
Igor Parra <igor.parra@rpinfo.com.br>
2021-01-19 09:55:25 -0300

    Agendamentos OK, criar dashboard para o barbeiro

---
+++ backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,52 @@
+import { injectable, inject } from 'tsyringe';
+
+import AppError from '@shared/errors/AppError';
+
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
+import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+
+interface IRequest {
+  user_id: string;
+  avatarFilename: string;
+}
+
+@injectable()
+class UpdateUserAvatarService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
+  ) {}
+
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const user = await this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('Only authenticated user can change avatar', 401);
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+
+      try {
+        await fs.promises.stat(userAvatarFilePath);
+        await fs.promises.unlink(userAvatarFilePath);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    const fileName = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = fileName;
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
